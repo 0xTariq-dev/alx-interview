26faab693006948fb1fc4ce2c5df3add091f01c6
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-25 23:33:47 +0300

    fix: UTF-8 Validation method #1

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,24 +9,25 @@ def validUTF8(data):
     Checks if a list of integers represents a valid UTF-8 encoding.
     """
     byte_count = 0
-    for byte in data:
-        if type(byte) != int or byte < 0 or byte > 0x10ffff:
+    for i in range(len(data)):
+        byte = data[i]
+
+        if type(byte) != int or byte < 0 or byte > 255:
             return False
 
-        if not (0 <= byte <= 0x10ffff):
-            return False
-        if byte >> 7 == 0:
-            byte_count = 0
-        elif byte >> 5 == 0b110:
-            byte_count = 1
-        elif byte >> 4 == 0b1110:
-            byte_count = 2
-        elif byte >> 3 == 0b11110:
-            byte_count = 3
-        else:
-            return False
-        for _ in range(byte_count):
-            if data[1 + byte_count - _] >> 6 != 0b10:
+        if byte_count == 0:
+            if byte >> 7 == 0:
+                continue
+            elif byte >> 5 == 0b110:
+                byte_count = 1
+            elif byte >> 4 == 0b1110:
+                byte_count = 2
+            elif byte >> 3 == 0b11110:
+                byte_count = 3
+            else:
                 return False
-        byte_count = 0
-    return True
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            byte_count -= 1
+    return byte_count == 0
