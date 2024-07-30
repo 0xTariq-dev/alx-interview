019c621ed07327a798796579fdef0a3353aa2222
Tariq Abu-Elhamd <132394314+0xTariq-dev@users.noreply.github.com>
2024-07-30 15:21:19 +0300

    Doc: 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
+""" Making Change Solution Module """
+
 
 def makeChange(coins, total):
     """
     Calculate the minimum number of coins to meet the total change
-        
+
     Args:
         coins (list of integers): Available coins values
         total (int): The total change amount to be returned
-    
+
     Returns: The minimum number of coins to meet the total
         Or 0 if total eqauls 0
         Or -1 if total can't be by any number of coins
     """
-    if total == 0:
-        return 0
     if total < 0:
-        return -1
-    
+        return 0
+
     coins.sort(reverse=True)
-    
+
     min_coins = 0
     n = len(coins) - 1
     for coin in coins:
